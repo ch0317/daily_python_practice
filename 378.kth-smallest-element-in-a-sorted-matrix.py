5673e612bcd9c0a01434f613c61e256be7d6e22c
chen <chenhao03@sf.local>
2025-10-16 09:01:32 +0800

    daily 1016

---
+++ 378.kth-smallest-element-in-a-sorted-matrix.py
@@ -0,0 +1,67 @@
+# 给你一个 n x n 矩阵 matrix ，其中每行和每列元素均按升序排序，找到矩阵中第 k 小的元素。
+# 请注意，它是 排序后 的第 k 小元素，而不是第 k 个 不同 的元素。
+#
+# 你必须找到一个内存复杂度优于 O(n2) 的解决方案。
+#
+#
+#
+# 示例 1：
+#
+# 输入：matrix = [
+# [1,5,9],
+# [10,11,13],
+# [12,13,15]], k = 8
+# 输出：13
+# 解释：矩阵中的元素为 [1,5,9,10,11,12,13,13,15]，第 8 小元素是 13
+# 示例 2：
+#
+# 输入：matrix = [[-5]], k = 1
+# 输出：-5
+#
+#
+# 提示：
+#
+# n == matrix.length
+# n == matrix[i].length
+# 1 <= n <= 300
+# -109 <= matrix[i][j] <= 109
+# 题目数据 保证 matrix 中的所有行和列都按 非递减顺序 排列
+# 1 <= k <= n2
+import heapq
+from typing import List
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+        pq = [(matrix[i][0], i, 0) for i in range(n)]
+        heapq.heapify(pq)
+
+        for _ in range(k - 1):
+            v, i, c = heapq.heappop(pq)
+
+            if c + 1 < n:
+                heapq.heappush(pq, (matrix[i][c + 1], i, c + 1))
+
+        return heapq.heappop(pq)[0]
+
+
+if __name__ == "__main__":
+    s = Solution()
+
+    matrix1 = [
+        [1, 5, 9],
+        [10, 11, 13],
+        [12, 13, 15]
+    ]
+    k1 = 8
+    print(s.kthSmallest(matrix1, k1))  # 预期输出：13
+
+    matrix2 = [[-5]]
+    k2 = 1
+    print(s.kthSmallest(matrix2, k2))  # 预期输出：-5
+
+    matrix3 = [
+        [1, 2],
+        [1, 3]
+    ]
+    k3 = 2
+    print(s.kthSmallest(matrix3, k3))  # 预期输出：1
